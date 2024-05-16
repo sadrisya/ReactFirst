1b1f6b3916c1fb2585d6ed52c90386bd393c9eef
sadrisya <sadrisyalohid@gmail.com>
2024-05-16 11:08:41 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <div className='app-footer'>
+      copyright codemalayalam.in All right Reserved
+    </div>
+    )}
+    export default Footer;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import List from './list/List';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div className='app'>
+   <Header/>
+    <div className='app-body'>
+    <List/>
+    </div>
+    <Footer />
+  </div>
+
+);
+
+
